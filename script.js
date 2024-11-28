a104f6b1732dab3b851ca762141c5cfdfe80b63d
Muzammil Usman <uraza2520@gmail.com>
2024-11-29 03:08:15 +0500

    working on delete task

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var userInp = document.getElementById("input");
 var toDo = document.getElementById("toDo");
+var li;
 
 function addTask() {
   var tasksHandler = document.createElement("div");
@@ -15,8 +16,12 @@ function addTask() {
   editBtn.innerText = "Edit";
   tasksHandler.appendChild(editBtn);
   var deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("onclick", "deleteTask(event)");
   deleteBtn.innerText = "Delete";
   tasksHandler.appendChild(deleteBtn);
-
   userInp.value = "";
+}
+
+function deleteTask(e) {
+  return e.target.parentElement.remove();
 }
