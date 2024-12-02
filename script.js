40dcde29682f05bf62f74500433fe26ba09d979b
Muzammil Usman <uraza2520@gmail.com>
2024-12-02 23:34:01 +0500

    responsiveness and local storage remaining

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var userInp = document.getElementById("input");
 var toDo = document.getElementById("toDo");
 var li;
 var hideInp;
+var doneBtn;
 
 function addTask() {
+  if (userInp.value === "") {
+    alert("Please enter your task");
+    return;
+  }
+
   var tasksHandler = document.createElement("div");
   tasksHandler.setAttribute("class", "taskHandler");
   toDo.appendChild(tasksHandler);
@@ -14,20 +20,22 @@ function addTask() {
   hideInp.setAttribute("type", "text");
   hideInp.setAttribute("class", "hide");
   hideInp.setAttribute("autofocus", "autofocus");
-
   tasksHandler.appendChild(hideInp);
   var editBtn = document.createElement("button");
   editBtn.setAttribute("onclick", "editTask(event)");
   editBtn.innerText = "Edit";
+  editBtn.setAttribute("class", "editBtn");
   tasksHandler.appendChild(editBtn);
-  var saveBtn = document.createElement("button");
-  saveBtn.setAttribute("onclick", "saveEditTask(event)");
-  saveBtn.innerText = "Save";
-  tasksHandler.appendChild(saveBtn);
   var deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("onclick", "deleteTask(event)");
   deleteBtn.innerText = "Delete";
+  deleteBtn.setAttribute("class", "deleteBtn");
   tasksHandler.appendChild(deleteBtn);
+  doneBtn = document.createElement("button");
+  doneBtn.innerText = "Done";
+  doneBtn.setAttribute("onclick", "doneTask(event)");
+  doneBtn.setAttribute("class", "doneBtn");
+  tasksHandler.appendChild(doneBtn);
   userInp.value = "";
   return;
 }
@@ -43,7 +51,39 @@ function editTask(e) {
     e.target.previousElementSibling.previousElementSibling.innerText;
   e.target.previousElementSibling.focus();
   e.target.previousElementSibling.previousElementSibling.style.display = "none";
+  e.target.innerText = "save";
+  e.target.nextElementSibling.innerText = "cancel";
+  e.target.nextElementSibling.setAttribute("onclick", "cancelTask(event)");
+  e.target.setAttribute("onclick", "saveEditTask(event)");
   return;
 }
 
-function saveEditTask(e) {}
+function saveEditTask(e) {
+  e.target.previousElementSibling.previousElementSibling.style.display =
+    "block";
+  e.target.previousElementSibling.style.display = "none";
+  e.target.innerText = "edit";
+  e.target.setAttribute("onclick", "editTask(event)");
+  e.target.nextElementSibling.innerText = "delete";
+  e.target.previousElementSibling.previousElementSibling.innerText =
+    e.target.previousElementSibling.value;
+  return;
+}
+
+function doneTask(e) {
+  e.target.style.display = "none";
+  e.target.previousElementSibling.style.display = "none";
+  e.target.previousElementSibling.previousElementSibling.style.display = "none";
+  e.target.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.style.textDecorationLine =
+    "line-through";
+}
+
+function cancelTask(e) {
+  e.target.previousElementSibling.innerText = "edit";
+  e.target.previousElementSibling.setAttribute("onclick", "editTask(event)");
+  e.target.previousElementSibling.previousElementSibling.style.display = "none";
+  e.target.previousElementSibling.previousElementSibling.previousElementSibling.style.display =
+    "block";
+  e.target.innerText = "delete";
+  e.target.setAttribute("onclick", "deleteTask(event)");
+}
